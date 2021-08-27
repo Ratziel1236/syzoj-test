516a152e3e3ff48a7dc9574fc191224cd0354b1a
hotwords123 <hotwords123@gmail.com>
2021-08-27 18:36:50 +0800

    Allow calendar using server as proxy

--- modules/calendar.js
+++ modules/calendar.js
@@ -1,19 +1,44 @@
+
+const request = require('request-promise');
+
 app.get('/calendar', async (req, res) => {
-  if (!syzoj.config.calendar.enabled) {
+  try {
+    const config = syzoj.config.calendar;
+
+    if (!config.enabled)
+      throw new ErrorMessage("未开启日历功能。");
+    if (!config.api_url)
+      throw new ErrorMessage("日历配置有误，请联系管理员。");
+
+    res.render('calendar');
+  } catch (err) {
     res.render('error', {
-      err: '未开启日历功能。'
+      err: err.message
     });
-  } else if (!syzoj.config.calendar.api_url) {
+  }
+});
+
+app.get('/calendar/proxy', async (req, res) => {
+  try {
+    const config = syzoj.config.calendar;
+
+    if (!config.enabled)
+      throw new ErrorMessage("未开启日历功能。");
+    if (!config.api_url)
+      throw new ErrorMessage("日历配置有误，请联系管理员。");
+    if (!config.self_proxy)
+      return res.redirect(config.api_url);
+
+    const result = await request({
+      uri: config.api_url,
+      timeout: 2000,
+      json: true
+    });
+
+    res.send(result);
+  } catch (err) {
     res.render('error', {
-      err: '日历配置有误，请联系管理员。'
+      err: err.message
     });
-  } else {
-    try {
-        res.render('calendar');
-    } catch (err) {
-      res.render('error', {
-        err: err
-      });
-    }
   }
 });
